75be40e6ef9e4517f9dd71145edd3abf40dd4d0c
Emanuele Serrao <Emanuele.Serrao@techedgegroup.com>
2021-04-04 15:51:42 +0200

    gen all update

--- stitchAppFramework.0.2.12.js
+++ stitchAppFramework.0.2.12.js
@@ -153,7 +153,6 @@ var StitchDefaultPages = [
                   "form_page"
                ]
             ],
-            "node_afterinit":"loginSetup",
             "node_childs":[
                {
                   "node_type":"div",
